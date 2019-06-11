9c49547e58f2967dca88d6fa79e1d47c0d713ea9
Jace Babin <jbabin91@gmail.com>
2019-06-11 09:27:24 -0700

    Started working on the author repository

---
+++ author/repository/mysql_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/jbabin91/go-clean-arch/author"
+	"github.com/jbabin91/go-clean-arch/models"
+)
+
+type MysqlAuthorRepo struct {
+	DB *sql.DB
+}
+
+func (m *MysqlAuthorRepo) getOne(ctx context.context, query string, args ...interface{}) (*models.Author, error) {
+	stmt, err := m.DB.PrepareContext(ctx, query)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	row := stmt.QueryRowContext(ctx, args...)
+	a := &models.Author{}
+
+	err = row.Scan(
+		&a.ID,
+		&a.Name,
+		&a.CreatedAt,
+		&a.UpdatedAt,
+	)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func (m *MysqlAuthorRepo) GetByID(ctx context.Context, id uint64) (*models.Author, error) {
+	query := `SELECT id, name, created_at, updated_at FROM author WHERE id=?`
+	return m.getOne(ctx, query, id)
+}
